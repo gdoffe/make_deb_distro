c8f1ece007ed2d68d7072e2f8f794f0d29c1a41b
Gilles DOFFE <g.doffe@gmail.com>
2013-11-24 20:06:38 +0100

    Add 'Installed-Size' field to debian package control file.

--- packaging/make-package.sh
+++ packaging/make-package.sh
@@ -11,6 +11,8 @@ cp -Rf ${SRCDIR}/make-deb-distro/profiles/ ${PKGDIR}/etc/make-deb-distro/
 cp -Rf ${SRCDIR}/make-deb-distro/scripts ${PKGDIR}/usr/share/make-deb-distro/
 cp -Rf ${SRCDIR}/make-deb-distro/templates ${PKGDIR}/usr/share/make-deb-distro/
 
+installed_size=$(du -sb ${PKGDIR} | cut -s -f1)
+
 echo "Package: make-deb-distro
 Version: 1.0
 Section: Utilities
@@ -19,6 +21,7 @@ Architecture: all
 Depends: bash,realpath,whois,qemu-user-static,qemu-system,binfmt-support,squashfs-tools,extlinux,mbr,debootstrap
 Maintainer: Gilles DOFFE <gdoffe@gmail.com>
 Description: Build a custom ubuntu/debian distro.
-Homepage: https://github.com/gdoffe/make_deb_distro" > ${PKGDIR}/DEBIAN/control
+Homepage: https://github.com/gdoffe/make_deb_distro
+Installed-Size: ${installed_size}" > ${PKGDIR}/DEBIAN/control
 
 dpkg-deb --verbose --build ${PKGDIR}
