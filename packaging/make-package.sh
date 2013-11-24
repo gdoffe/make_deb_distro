d38fac64cb49e9cd77670b24067cae1a27b09206
Gilles DOFFE <g.doffe@gmail.com>
2013-11-24 22:07:55 +0100

    Update copyrights.

--- packaging/make-package.sh
+++ packaging/make-package.sh
@@ -30,4 +30,37 @@ Description: Build a custom ubuntu/debian distro.
 Homepage: https://github.com/gdoffe/make_deb_distro
 Installed-Size: ${installed_size}" > ${PKGDIR}/DEBIAN/control
 
+echo "
+Format: http://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: make-deb-distro
+Source: https://github.com/geonobot/make_deb_distro
+
+Files: *
+Copyright: Copyright (C) 2013 Gilles DOFFE <gdoffe@gmail.com>
+License: GPL-3
+ This program is free software; you can redistribute it
+ and/or modify it under the terms of the GNU General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later
+ version.
+ .
+ This program is distributed in the hope that it will be
+ useful, but WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the GNU General Public License for more
+ details.
+ .
+ You should have received a copy of the GNU General Public
+ License along with this package; if not,
+ see <http://www.gnu.org/licenses/>.
+ .
+ On Debian systems, the full text of the GNU General Public
+ License version 3 can be found in the file
+ /usr/share/common-licenses/GPL-3.
+
+Files: debian/*
+Copyright: Copyright 2013 Gilles DOFFE <gdoffe@gmail.com>
+License: GPL-3
+ [LICENSE TEXT]" > ${PKGDIR}/DEBIAN/copyright
+
 dpkg-deb --verbose --build ${PKGDIR}
